e73bbe441c3c247169268c8b70e7cc85f48245ce
hd2yao <1054290355@qq.com>
2025-09-08 14:13:32 +0800

    feat(service/user): 使用 goctl 命令生成 model api rpc 文件

---
+++ service/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"github.com/hd2yao/micro-mall/service/user/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
